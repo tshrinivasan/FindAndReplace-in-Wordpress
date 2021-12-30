6ca00e633fb7fce2e2a92f2578e089ffd166be0d
anitha ganesan <anithagk12@gmail.com>
2021-12-30 12:56:22 +0530

    Add files via upload

---
+++ Wordpress.py
@@ -0,0 +1,3 @@
+from wordpress import API
+from wordpress_xmlrpc import Client, WordPressPost
+wp = Client('https://www.wordpress.com/home/python.sport.blog', 'anithagk12@gmail.com','Anuvinayaga@12')
